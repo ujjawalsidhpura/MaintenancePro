5a88a8d1667f029db569e3cb5e4d8d2d1e04a52e
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2021-11-19 01:51:13 +0530

    fake data and routes created from api for front end

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -1,9 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
+const inventory = [
+  {
+    _id: 1,
+    _category: 'Hardware',
+    item: 'bolt 2 inch',
+    price_item: 1,
+    quantity: 100
+  },
+  {
+    _id: 2,
+    _category: 'Hardware',
+    item: 'bolt 5 inch',
+    price_item: 3.5,
+    quantity: 50
+  },
+  {
+    _id: 3,
+    _category: 'Interior',
+    item: 'carpet tiles',
+    price_item: 45,
+    quantity: 2000
+  },
+  {
+    _id: 4,
+    _category: 'Interior',
+    item: 'Paint 3L jugs',
+    price_item: 60,
+    quantity: 15
+  },
+  {
+    _id: 5,
+    _category: 'Tools',
+    item: 'Landmover john deere 2010',
+    price_item: 7000,
+    quantity: 1
+  }
+]
+
 /* GET users listing. */
 router.get('/', function (req, res) {
-  res.send({ "inventory": "1" });
+  res.send(inventory);
 });
 
 module.exports = router;
--- backend/routes/workorder.js
+++ backend/routes/workorder.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const workorders = [
+  {
+    _id: '1',
+    email: 'abc@abc.com',
+    name: 'abc',
+    description: 'test workorder',
+    created_on: Date.now(),
+    time_started: null,
+    time_completed: null,
+    duration: null
+  },
+  {
+    _id: '2',
+    email: 'cde@abc.com',
+    name: 'cde',
+    description: 'test workorder 2',
+    created_on: Date.now(),
+    time_started: null,
+    time_completed: null,
+    duration: null
+  }
+]
+
+
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.send({ "workorder": "1" })
+  res.send(workorders)
 });
 
 module.exports = router;
