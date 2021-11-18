112a9bd82aca26b74fd3c194c37b4cd711ac75ea
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2021-11-19 02:20:42 +0530

    fetched our API data and store it in state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,38 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 function App() {
+
+  const [state, setState] = useState({
+    workorder: null,
+    inventory: null
+  })
+
+  useEffect(() => {
+    Promise.all(
+      [
+        axios.get('/workorder'),
+        axios.get('/inventory')
+      ]
+    )
+      .then((all) => {
+        const workorder = all[0].data
+        const inventory = all[1].data
+        setState(prev => ({
+          ...prev, workorder, inventory
+        }))
+      })
+  }, [])
+
 
   return (
     <div className="App">
 
     </div>
-  );
+  )
 }
 
 export default App;
