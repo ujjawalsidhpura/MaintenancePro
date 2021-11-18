fa57b276d86e2d33f44ff9affbee15af6d391b13
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2021-11-18 22:53:37 +0530

    dotenv lib set up for env variables, .env file setup, API set at 3001, React proxy set at 3001 and react server can run on 3002 or 3000 , .gitinore in backend folder

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const PORT = process.env.PORT
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(PORT, console.log(`API listening at port ${PORT}`))
 
 module.exports = app;
