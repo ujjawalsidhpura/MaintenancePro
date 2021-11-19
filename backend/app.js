f54cc03790e4695b5ed369166d1533f5087b3a2e
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2021-11-19 19:15:16 +0530

    Code refactored for GET routes from mongodb

--- backend/app.js
+++ backend/app.js
@@ -2,24 +2,27 @@
 require('dotenv').config()
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const app = express();
 const debug = require('debug')('backend:server');
 const http = require('http');
 const port = process.env.PORT || '3000';
 const server = http.createServer(app);
+const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+const logger = require('morgan');
 
 //Middleware setup
 app.set('port', port);
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Mongo Connections
+//Mongo Connection
 const mongoDb = require('./mongoDb');
+
 mongoDb.connectToServer(function (err) {
   //App goes online once this callback occurs
 
@@ -34,15 +37,14 @@ mongoDb.connectToServer(function (err) {
     next(createError(404));
   });
 
+  //Handle 500
   app.use(function (err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
+    res.status(err.status || 500).send('Error')
   });
 
 })
-
 
 //Start Server
 server.listen(port, () => {
--- backend/mongoDb.js
+++ backend/mongoDb.js
@@ -6,13 +6,19 @@ const MongoClient = require('mongodb').MongoClient;
 let _db;
 
 module.exports = {
+
   connectToServer: function (callback) {
     MongoClient.connect(CONNECTION_URL, function (err, client) {
+
       _db = client.db(DATABASE_NAME);
+      console.log('Connected to database: ' + DATABASE_NAME);
       return callback(err);
+
     });
   },
+
   getDb: function () {
     return _db;
   }
+
 };
