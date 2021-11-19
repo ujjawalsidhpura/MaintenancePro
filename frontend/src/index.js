23b759608c52fcc8b7528aaff655b3a5e3f9c347
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-19 14:32:09 -0500

    set up for auth0 authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Auth0ProviderWithHistory>
+        <App />
+      </Auth0ProviderWithHistory>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
