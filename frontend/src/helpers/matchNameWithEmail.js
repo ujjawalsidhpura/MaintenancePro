980c1d9112d4b413b4b6379a68a765bd0dab1714
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-21 00:34:23 -0500

    filter workorders for each technician

---
+++ frontend/src/helpers/matchNameWithEmail.js
@@ -0,0 +1,10 @@
+export  function matchNameWithEmail(name, email){
+  const username = email.split("@")[0];
+  for (const char of name){
+    if (!username.includes(char.toLowerCase())) return false
+  }
+  return true;
+}
+
+// name = Shuhao Zhang email = shuhao.qgg.zhang@gmail.com return true
+
