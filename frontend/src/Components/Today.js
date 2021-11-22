d8e13408adddafaa1afc7d180dd0ec8550a420cc
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-22 18:34:26 -0500

    refactor the pie chart

--- frontend/src/Components/Today.js
+++ frontend/src/Components/Today.js
@@ -1,7 +1,6 @@
 import WorkOrderItems from "./WorkOrderItems"
 import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
-
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Legend} from 'recharts';
 export default function Today(props) {
   console.log("today at glance",props.today);
   const parsedWorkOrders = props.today.map(workOrder => {
@@ -46,14 +45,12 @@ export default function Today(props) {
   const RADIAN = Math.PI / 180;
 
   const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.4;
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
   
     return (
-      <text x={x} y={y+10} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-        {dataForPieChart[index].name}
-        {'\r\n'}
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
         {`${(percent * 100).toFixed(0)}%`}
       </text>
       
@@ -65,23 +62,24 @@ export default function Today(props) {
       <h1>Today's Work Orders</h1>
       <h2>There are {props.today.length} unfinished tasks</h2>
       { summaryForEachTechnicain }
-      <PieChart width={400} height={400}>
+      <ResponsiveContainer width="100%" height={250}>
+      <PieChart title="hello" width={400} height={400}>
+      <Legend wrapperStyle={{top: 0, left: 25}}/>
         <Pie 
         isAnimationActive={false}
         label={renderCustomizedLabel}
         labelLine={false}
-        cx={200}
-        cy={200}
+        cx={300}
+        cy={120}
         data={dataForPieChart} 
         dataKey="unfinishedtasks" 
-        outerRadius={150} 
+        outerRadius={100} 
         fill="#fff" 
         >
-      {dataForPieChart.map(eachPie => {
-          return (<h1>{eachPie.unfinishedtasks}</h1>)
-      })}
+
         </Pie>
       </PieChart>
+      </ResponsiveContainer>
       {parsedWorkOrders}
     </div>
   )
