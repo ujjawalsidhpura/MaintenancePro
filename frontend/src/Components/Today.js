60ca70b7cbdd2f90facac40d366c3e65b441105d
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-22 16:54:12 -0500

    summary for today at glance

--- frontend/src/Components/Today.js
+++ frontend/src/Components/Today.js
@@ -8,16 +8,39 @@ export default function Today(props) {
 
   const workOrderByTechnician = function(workorders, technician) {
     return workorders.filter(workorder => 
-        workorder.technician === technician
+        workorder.technician == technician
       )
   }
 
-  console.log(workOrderByTechnician(parsedWorkOrders, "Shuhao Zhang"))
+  const eachTechnician = function(workorders) {
+    const output = [];
+    for (const workorder of workorders) {
+      if (!output.includes(workorder.technician)) {
+        output.push(workorder.technician);
+      }
+    }
+    return output;
+  }
+
+  const technicianArray = eachTechnician(props.today);
+  
+  const summaryForEachTechnicain = technicianArray.map(technician =>
+    {
+      return (<h2>{technician} has {workOrderByTechnician(props.today, technician).length} unfinished tasks</h2>)
+    }
+  );
+  for (const technician of technicianArray) {
+    console.log(`${technician} has ${workOrderByTechnician(props.today, technician).length} unfinished tasks`)
+  }
+  // console.log("eachtechnician",eachTechnician(props.today));
+  // console.log("workOrderByTechnician",workOrderByTechnician(props.today, "Ujjawal Sidhpura"))
   return (
     <div className="workorder-container">
       <h1>Today's Work Orders</h1>
       <h2>There are {props.today.length} unfinished tasks</h2>
+      { summaryForEachTechnicain }
       {parsedWorkOrders}
+      
     </div>
   )
 }
