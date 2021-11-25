cb06e6bd2da99af0aa942fb7bff22ba75fe2bea2
Ebuka Moneme <camoneme@gmail.com>
2021-11-25 17:13:04 -0500

    Style summary page

--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -291,86 +291,100 @@ export default function Summary(props) {
 
 
   return (
+		<>
+			<div className="summary-page" >
+				<h1 className="title summary-header">Summary of <Dropdown />: </h1>
+				<div className="summary-content">
 
-    <div className="summary-page" >
-      <h1 className="title">Summary of <Dropdown />: </h1>
-      <br />
-      <h2><i>{mostEfficientMonth}</i></h2>
-      <br />
-      <h2><i>{mostEfficientTechnician}</i></h2>
-      <br />
-      <h2><i>{mostHardWorkingTechnician}</i></h2>
-      <br />
-      <h2><i>{busiestMonth}</i></h2>
+					<div className="card summary-field">
+						<div className="summary-paragraph">
+							<p class="tag is-light">Most Efficient Month</p>
+							<h2><i>{mostEfficientMonth}</i></h2>
+						</div>
+						<div className="summary-paragraph">
+							<p class="tag is-light">Most Efficient Technician</p>
+							<h2><i>{mostEfficientTechnician}</i></h2>
+						</div>
+						<div className="summary-paragraph">
+							<p class="tag is-light">Most Completed Workorders</p>
+							<h2><i>{mostHardWorkingTechnician}</i></h2>
+						</div>
+						<div className="summary-paragraph">
+							<p class="tag is-light">Busiest Month</p>
+							<h2><i>{busiestMonth}</i></h2>
+						</div>
+					</div>
 
-			<div className="card">
-				<h1><b>Work Order Summary for year {year}</b></h1>
-				<ComposedChart
-					width={500}
-					height={400}
-					data={workOrderSummary}
-					margin={{
-						top: 20,
-						right: 20,
-						bottom: 20,
-						left: 20
-					}}
-				>
-					<CartesianGrid stroke="#f5f5f5" />
-					<XAxis dataKey="month" />
-					<YAxis />
-					<Tooltip />
-					<Legend wrapperStyle={{ top: 0, left: 25 }} />
-					<Area type="monotone" dataKey="total_workorder_created" fill="#8884d8" stroke="#8884d8" />
-					<Bar dataKey="completed_workorder" barSize={20} fill="#413ea0" />
-					<Line type="monotone" dataKey="unfinished_workorder" stroke="#ff7300" />
-				</ComposedChart>
-			</div>
-
-			<div className="card">
-				<h1><b>Work Orders Completed by Technicians for year {year}</b></h1>
-				<ResponsiveContainer 
-					width="100%" 
-					height={250}
-				>
-					<PieChart width={400} height={400}>
-						<Legend wrapperStyle={{ top: 0, left: 25 }} />
-						<Pie
-							isAnimationActive={false}
-							label={renderCustomizedLabel}
-							labelLine={false}
-							cx={300}
-							cy={120}
-							data={dataForEachTech}
-							dataKey="completed_tasks"
-							outerRadius={100}
-							fill="#fff"
+					<div className="card summary-field">
+						<h1><b>Work Order Summary for year {year}</b></h1>
+						<ComposedChart
+							width={500}
+							height={400}
+							data={workOrderSummary}
+							margin={{
+								top: 20,
+								right: 20,
+								bottom: 20,
+								left: 20
+							}}
 						>
-						</Pie>
-					</PieChart>
-				</ResponsiveContainer>
-			</div>
+							<CartesianGrid stroke="#f5f5f5" />
+							<XAxis dataKey="month" />
+							<YAxis />
+							<Tooltip />
+							<Legend wrapperStyle={{ top: 0, left: 25 }} />
+							<Area type="monotone" dataKey="total_workorder_created" fill="#8884d8" stroke="#8884d8" />
+							<Bar dataKey="completed_workorder" barSize={20} fill="#413ea0" />
+							<Line type="monotone" dataKey="unfinished_workorder" stroke="#ff7300" />
+						</ComposedChart>
+					</div>
 
-			<div className="card">
-				<h1><b>Avg Time Spent on Workorder for each Technician</b></h1>
-				<BarChart
-					width={500}
-					height={300}
-					data={dataForEachTech}
-					margin={{
-						top: 5,
-						right: 30,
-						left: 20,
-						bottom: 5
-					}}
-				>
-					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="name" />
-					<YAxis />
-					<Tooltip />
-					<Bar dataKey="avg_duration" fill="#8884d8" />
-				</BarChart>
+					<div className="card summary-field">
+						<h1><b>Work Orders Completed by Technicians for year {year}</b></h1>
+						<ResponsiveContainer 
+							width="100%" 
+							height={250}
+						>
+							<PieChart width={400} height={400}>
+								<Legend wrapperStyle={{ top: 10, left: 25 }} />
+								<Pie
+									isAnimationActive={false}
+									label={renderCustomizedLabel}
+									labelLine={false}
+									cx={300}
+									cy={120}
+									data={dataForEachTech}
+									dataKey="completed_tasks"
+									outerRadius={100}
+									fill="#fff"
+								>
+								</Pie>
+							</PieChart>
+						</ResponsiveContainer>
+					</div>
+
+					<div className="card summary-field">
+						<h1><b>Avg Time Spent on Workorder for each Technician</b></h1>
+						<BarChart
+							width={500}
+							height={300}
+							data={dataForEachTech}
+							margin={{
+								top: 5,
+								right: 30,
+								left: 20,
+								bottom: 5
+							}}
+						>
+							<CartesianGrid strokeDasharray="3 3" />
+							<XAxis dataKey="name" />
+							<YAxis />
+							<Tooltip />
+							<Bar dataKey="avg_duration" fill="#8884d8" />
+						</BarChart>
+					</div>
+				</div>
 			</div>
-    </div>
+		</>
   )
 }
