a53de0a13837076f4daafab2b425b3193673c6a7
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2021-11-26 22:06:35 +0530

    secure api endpoint in frontend

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from 'react-router-dom'
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(
   <React.StrictMode>
