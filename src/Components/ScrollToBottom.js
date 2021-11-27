9d5ccf9035d0e480af4aa0929afdb2bd34e76114
Ebuka Moneme <camoneme@gmail.com>
2021-11-27 09:58:52 -0500

    Remove comments

--- src/Components/ScrollToBottom.js
+++ src/Components/ScrollToBottom.js
@@ -3,7 +3,6 @@ import { useLocation } from 'react-router-dom';
 
 const ScrollToBottom = ({ children }, props) => {
   const { pathname } = useLocation();
-	console.log("Scroll children", children)
 
   useEffect(() => {
     window.scrollTo(100, 10000);
