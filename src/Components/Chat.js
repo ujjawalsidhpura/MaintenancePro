68625887a9f1aaa17845cb7bd3bda8193f83d71b
Ebuka Moneme <camoneme@gmail.com>
2021-11-27 11:31:14 -0500

    Add scroll to bottom when others send a message

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -17,6 +17,7 @@ export default function Chat(props) {
           setApplicationData(prev => ({
             ...prev, messages: [...res.data]
           }))
+					window.scrollTo(100, 10000);
         });
     })
   }, [setApplicationData, socket]);
